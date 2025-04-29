fe381cf2ab2f70560d5cf12df582862c6258468b
Steve Swinsburg <steve.swinsburg@gmail.com>
2025-04-29 14:36:49 +1000

    Update to add basic syntax highlighting and the skip control

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,10 @@ function Footer() {
     <footer className="bg-light text-center py-3 mt-5">
       <Container>
         <small>
-          <strong>StepRunner</strong> — Made by humans 👨 for gherkins 🥒. View on{' '}
+          <strong>StepRunner</strong> - Made by humans 👨 for gherkins 🥒 - View on{' '}
           <a href="https://github.com/steveswinsburg/steprunner" target="_blank" rel="noopener noreferrer">
             GitHub
           </a>
-          .
         </small>
       </Container>
     </footer>
